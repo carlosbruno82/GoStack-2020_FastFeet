81b81306a25cc4ba146bc70b29521d6ad40b653d
Carlos Bruno <carlao.linux@gmail.com>
2020-02-19 23:34:23 -0300

    gestao

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5760d34127d9a735e5123efecc964a0a',
+  expirenIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ mensage: 'Hello World' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
